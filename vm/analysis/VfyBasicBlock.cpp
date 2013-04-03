7a9d3d81937a48cf405bfbd1cabd41509843bbbd
You Kim <you.kim72@gmail.com>
2013-04-04 04:24:20 +0900

    Fix some leaks in VfyBasicBlock & BitVector.
    Change-Id: I20050135588fc96268911b8ab690b04c8ad949bc

--- vm/analysis/VfyBasicBlock.cpp
+++ vm/analysis/VfyBasicBlock.cpp
@@ -544,6 +544,7 @@ void dvmFreeVfyBasicBlocks(VerifierData* vdata)
             continue;
 
         dvmPointerSetFree(block->predecessors);
+        dvmFreeBitVector(block->liveRegs);
         free(block);
     }
 
